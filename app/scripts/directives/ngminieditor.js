ab4eec2254b1728e177bcc64d8eed47ae477bec2
yang heng <546192952@qq.com>
2015-01-04 18:24:11 +0800

    添加了directive

---
+++ app/scripts/directives/ngminieditor.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name appApp.directive:ngMiniEditor
+ * @description
+ * # ngMiniEditor
+ */
+angular.module('appApp')
+  .controller('ngMiniCtr',['$scope',function($scope){
+       var Ctr=this,
+        editors=Ctr.editors=$scope.editors=[];
+        Ctr.addEditors=function(editor){
+            editors.push(editor);
+        }
+        Ctr.removeEditors=function(editor){
+            var index = editors.indexOf(editor);
+            editors.splice(index, 1);
+        }
+}])
+  .directive('ngMiniEditor', function () {
+    return {
+      templateUrl: 'views/editorset.html',
+      restrict: 'E',
+      transclude:true,
+      controller:'ngMiniCtr',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the ngMiniEditor directive');
+      }
+    };
+  });
